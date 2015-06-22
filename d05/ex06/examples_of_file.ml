3a52642f0c9be6a06857c8728f86f2e44358499d
vikingeff <mail.leger@gmail.com>
2015-06-22 20:22:47 +0200

    updates at last motherufcker

--- d05/ex06/examples_of_file.ml
+++ d05/ex06/examples_of_file.ml
@@ -1,7 +1,7 @@
 (* ************************************************************************** *)
 (*                                                                            *)
 (*                                                        :::      ::::::::   *)
-(*   eu_dist.ml                                         :+:      :+:    :+:   *)
+(*   examples_of_file.ml                                :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
 (*   By: gleger <marvin@42.fr>                      +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
@@ -9,25 +9,6 @@
 (*   Updated: 2015/06/22 18:08:28 by gleger           ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
-
-let fill_array arrayz size = 
-	Random.self_init ();
-	let rec generate_array index =
-		if index < size then
-			begin
-				let neg = Random.int 2 in
-				if neg = 0 then
-					begin
-						arrayz.(index) <- (Random.float 1.);
-						generate_array (index+1)
-					end
-				else
-					begin
-						arrayz.(index) <- (Random.float (-1.));
-						generate_array (index+1)
-					end
-			end
-	in generate_array 0
 
 let print_array arrayz size =
 	print_string "Point of coordinate [|";
@@ -52,38 +33,29 @@ let get_dist ar1 ar2 =
 		in sub 0 0.0
 	end
 
+let transform filez = 
+	let data = open_in filez in
+	let coordz = input_line data in
+	print_endline coordz;
+	close_in data
 
-let main dim =
-	if dim <= 0 then
-		raise (Invalid_argument "Error: Need a positive int value !")
-	else
-		begin
-			let coord1 = Array.make dim 0.0 in
-			let coord2 = Array.make dim 0.0 in
-			let coord3 = Array.make (dim+1) 0.0 in
-			print_endline ("There is "^(string_of_int dim)^" dimension.");
-			fill_array coord1 dim;
-			fill_array coord2 dim;
-			print_array coord1 dim;
-			print_array coord2 dim;
-			print_endline "The Euclidian distance between those two points is about :";
-			print_float (get_dist coord1 coord2);
-			print_char '\n';
-			print_float (get_dist coord1 coord3)
-		end
-	
+let main filez =
+	let lst = transform filez in
+	print_data 
+		
 	(* ************************************************************************** *)
 let () =
 	let argv = Array.to_list Sys.argv in
 	if (List.length argv) <= 1 then
-		print_endline "Error: Need a dimension number !"
+		print_endline "Error: Need the name of a file to work!"
 	else if (List.length argv) > 2 then
 		print_endline "Error: Need less args."
 	else
 		begin
 			try
-				main (int_of_string(List.nth argv 1))
+				main (List.nth argv 1)
 			with
+			| Sys_error err -> print_endline "Error: Yeah like that file exists ..."
 			| Invalid_argument err -> print_endline err
 			| Failure err -> print_endline "Error: Please give me at least a number."
 			| _ -> print_endline "Unknown error."			
