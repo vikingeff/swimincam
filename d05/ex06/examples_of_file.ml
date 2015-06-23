f358dcea8725e28e08250ac56990e650ab77efe4
vikingeff <mail.leger@gmail.com>
2015-06-23 12:45:56 +0200

    d05ex06

--- d05/ex06/examples_of_file.ml
+++ d05/ex06/examples_of_file.ml
@@ -10,15 +10,37 @@
 (*                                                                            *)
 (* ************************************************************************** *)
 
+type coordz = (float array * string)
+let is_lower c = c >= 'a' && c <= 'z'
+let is_digit c = c >= '0' && c <= '9'
+
+let get_left (left,_) = left
+let get_right (_,right) = right
+
 let print_array arrayz size =
-	print_string "Point of coordinate [|";
+	print_string "[|";
 	for i=0 to (size-1) do
 		print_string (string_of_float (Array.get arrayz i));
 		if i != (size-1) then
-			print_string ", "
+			print_string "; "
 		else
-			print_string "|]\n"
+			print_string "|]"
 	done
+
+let print_tuple src = print_array (get_left src) (Array.length (get_left src));print_string ", "; print_string ("\""^(get_right src)^"\"")
+
+let print_list lst =
+	print_string "{\n";
+	let rec print_tuples lst = match lst with
+		| [] -> print_endline ")\n}"
+		| last::[] -> print_string "[|";
+					print_tuple last;
+					print_endline ")\n}"
+		| first::suite -> print_string "(";
+						print_tuple first;
+						print_string ");\n";
+						print_tuples suite
+		in print_tuples lst
 
 let get_dist ar1 ar2 =
 	if Array.length ar1 != Array.length ar2 then
@@ -33,15 +55,79 @@ let get_dist ar1 ar2 =
 		in sub 0 0.0
 	end
 
-let transform filez = 
+let rev_list lst = 
+	let rec backward lst acc = match lst with
+		| [] -> []
+		| last::[] -> last::acc
+		| first::suite -> backward suite (first::acc)
+		in backward lst []
+
+let coma str = 
+	let rec loop nb index src =
+		if index < (String.length src) then
+			if (String.get src index) = ',' then
+				loop (nb+1) (index+1) src
+			else
+				loop (nb) (index+1) src
+		else
+			nb
+	in loop 0 0 str
+
+let sizes_lst lst =
+	let int_list = ref [] in
+	let rec get_sizes src = function
+		| [] -> []
+		| last::[] -> src:=(coma last)::!src; !src
+		| first::suite -> (src:=(coma first)::!src); get_sizes src suite
+	in get_sizes int_list lst
+
+let file_to_list data_lst filez =
 	let data = open_in filez in
-	let coordz = input_line data in
-	print_endline coordz;
-	close_in data
+	try
+		while true do
+			data_lst := input_line data :: !data_lst
+		done;
+	with
+		| End_of_file -> close_in data
+		| _ -> print_endline "Unknown error (file_to_list)."
+
+let transform filez = 
+	let data_lst = ref [] in
+	let size_lst = ref [] in 
+	file_to_list data_lst filez;
+	data_lst := rev_list !data_lst;
+	size_lst := sizes_lst !data_lst;
+	size_lst := !size_lst;
+	(* print_endline (List.nth !data_lst 0);
+	print_int (List.nth !size_lst 0);
+	print_char '\n'; *)
+	let lst_return = ref [] in
+	for j=0 to (List.length !data_lst)-1 do
+		let farray = Array.make (List.nth !size_lst j) 0.0 in
+		let str_list = Str.split (Str.regexp "[,]") (List.nth !data_lst j) in
+		for i=0 to (List.nth !size_lst 0)-1 do
+			farray.(i) <- float_of_string (List.nth str_list i)
+		done;
+		
+		lst_return := (farray, (List.nth str_list (List.nth !size_lst j)))::!lst_return;
+		(* print_float farray.(33);
+		print_string " ";
+		print_endline (List.nth str_list (List.nth !size_lst j)); *)
+	done;
+	print_endline (string_of_int(List.length !lst_return));
+	(* size_lst *)lst_return := rev_list (!lst_return);
+	lst_return
+	(* print_int (List.nth size_lst 0) *)
+	
 
 let main filez =
 	let lst = transform filez in
-	print_data 
+	if !lst = [] then
+		print_endline "empty";
+	print_list !lst
+	(*else *)
+	(* print_int (List.nth !lst 0) *)
+	(* print_data  *)
 		
 	(* ************************************************************************** *)
 let () =
@@ -57,7 +143,7 @@ let () =
 			with
 			| Sys_error err -> print_endline "Error: Yeah like that file exists ..."
 			| Invalid_argument err -> print_endline err
-			| Failure err -> print_endline "Error: Please give me at least a number."
-			| _ -> print_endline "Unknown error."			
+			| Failure err -> print_string "Error: Failure ('"; print_string err; print_endline "')"
+			| _ -> print_endline "Unknown error (main)."			
 		end
 
